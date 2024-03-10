f9ced3580f202f03cd49b176d0bd1c4324e2c9aa
Bruna Farina <82400814+BrunaFarina@users.noreply.github.com>
2024-03-10 18:36:40 +0100

    Update decision-making.py
    - the decision-making lib now supports multi genre movies, but also filter for two or more genres independently.
    - add description of each function and their parameters/return

--- decision-making.py
+++ decision-making.py
@@ -3,16 +3,6 @@
 #   Description: application for making decisions. It chooses from list of foods/movies/activities.
 #   Ref: https://developer.themoviedb.org
 
-
-### NOTES:
-# add more foods
-# replace loops for list comprehension
-# do I need the "genres" obj?
-# replace "elif user_request.lower() is "movie":" for "else"? - DONE
-# it's returning other movies other than the specified genre (ex: I want comedy; it also returns drama/comedy) - DONE
-# rank by rate instead of popularity - DONE (but realized rate returns shit movies. Back to popularity, but add a rate filter) - DONE
-#problem: some movies have similar/same name, it's better to return also the year (or director name) to have some more info - DONE
-
 import requests
 import random
 import json
@@ -43,47 +33,53 @@ tmdb_genres = {
     "Western": 37
 }
 
+API_KEY = "d3ebf605da6dc8d6ef411760eb67fa4c"
 def movie_names(genre, pages):
+    """
+    :param genre: list of genre ID (values from tmdb_genres dict)
+    :param pages: number of pages to be returned (each page returns 20 movies)
+    :return: a list of dictionaries. Each one for a movie
+    """
     movies = [] #empty list to store movie names
     merged_list = [] # empty list to store list of dictionaries
-
-    # remove the desirable movie genre from "genres" dict and store its key
-    if "," in genre:
-        desirable_genre = [tmdb_genres.pop(item) for item in genre]
-        desirable_genre = ",".join(str(id) for id in desirable_genre)
-    else:
-        desirable_genre = tmdb_genres.pop(genre)
     for page in range(0, pages): #the api returns only 1 page (loop to get other pages)
-        url = (f"https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page={page+1}&"
-               f"sort_by=popularity.desc&vote_average.gte=5&vote_average.lte=10&with_genres={desirable_genre}&"
+        url = (f"https://api.themoviedb.org/3/discover/movie?api_key={API_KEY}&include_adult=false&include_video=false&language=en-US&page={page+1}&"
+               f"sort_by=popularity.desc&vote_average.gte=5&vote_average.lte=10&with_genres={genre}&"
                f"without_genres={', '.join(str(x) for x in tmdb_genres.values())}") #convert dict values to a str
-        headers = {
-        "accept": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkM2ViZjYwNWRhNmRjOGQ2ZWY0MTE3NjBlYjY3ZmE0YyIsInN1YiI6IjY1ZTljMjQ1NmEyMjI3MDE4Njk2NmM5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dozyRSZ-_MPi-sWqQSuYQloqtotrDLMZM3Wq4lgdM-4"
-        }
-        response = requests.get(url, headers=headers)
+        response = requests.get(url)
         data = json.loads(response.text)
         merged_list = merged_list + data['results']
     return merged_list
 
 def decide_for_me(user_request):
-    if user_request.lower() == "food":
+    """
+
+    :param user_request: a sting indicating if the user wants a movie or a food.
+    :return: food/movie chosen randomly. For movies, the user should filter by genre(s)
+    """
+    if user_request.lower() == "food": #if you want to return food
         rnd_food = random.choice(foods)
         print("I think you should have", rnd_food, "today")
-
-    elif user_request.lower() == "movie":
-        genres = input("Which kind of movie do you like? ")
-
-        if "," in genres:
-            genres = list(genres.split(", "))
-            genres = [genre.title() for genre in genres]
-        else:
+    elif user_request.lower() == "movie": # if movie
+        genres = input("Which kind(s) of movie do you like? ")
+        if "," in genres: #in case you want more than 1 genre
+            criteria = input("Do you mean multi genre movies (both genres at the same movie) or movies with only one genre? (answer: 'multi genre' or 'mono genre') ")
+            if criteria == 'multi genre': #if you want, for example, genre 1 AND genre 2
+                genres = list(genres.split(", "))
+                genres = [genre.title() for genre in genres]
+                desirable_genre = [tmdb_genres.pop(item) for item in genres]
+                desirable_genre = ",".join(str(id) for id in desirable_genre)
+            else: # if you want genre 1 OR genre 2
+                genres = list(genres.split(", "))
+                genres = [genre.title() for genre in genres]
+                desirable_genre = [tmdb_genres.pop(item) for item in genres]
+                desirable_genre = "|".join(str(id) for id in desirable_genre)
+        else: #only one genre
             genres = genres.capitalize()
-
-        movie_list = movie_names(genres, 5) #each page returns 20 movies
+            desirable_genre = tmdb_genres.pop(genres)
+        movie_list = movie_names(desirable_genre, 10) #each page returns 20 movies
         rnd_movie = random.choice(movie_list)
         print(f"I think you should watch '{rnd_movie['title']}', released on {rnd_movie['release_date']}")
-
     else:
         print("This is not 'food' or 'movie'")
 
