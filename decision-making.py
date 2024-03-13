39fb3c2b4687ed43fc81316f8308589a2a66306f
Bruna Farina <82400814+BrunaFarina@users.noreply.github.com>
2024-03-13 09:17:45 +0100

    Update decision-making.py

--- decision-making.py
+++ decision-making.py
@@ -7,11 +7,8 @@ import requests
 import random
 import json
 
-# dict of foods
-foods = ["pasta", "pizza", "hamburger", "sushi", "indian_food", "mexican food"]
-
 # dict of movie genres (from TMDB)
-tmdb_genres = {
+TMDB_GENRES = {
     "Action": 28,
     "Adventure": 12,
     "Animation": 16,
@@ -34,56 +31,60 @@ tmdb_genres = {
 }
 
 API_KEY = "d3ebf605da6dc8d6ef411760eb67fa4c"
-def movie_names(genre, pages):
+def get_movie_names(genre_ids, pages):
     """
-    :param genre: list of genre ID (values from tmdb_genres dict)
-    :param pages: number of pages to be returned (each page returns 20 movies)
-    :return: a list of dictionaries. Each one for a movie
+    :param genre_ids: List of genre IDs
+    :param pages: Number of pages to fetch
+    :return: List of dictionaries, each containing movie information
     """
     movies = [] #empty list to store movie names
     merged_list = [] # empty list to store list of dictionaries
-    for page in range(0, pages): #the api returns only 1 page (loop to get other pages)
-        url = (f"https://api.themoviedb.org/3/discover/movie?api_key={API_KEY}&include_adult=false&include_video=false&language=en-US&page={page+1}&"
-               f"sort_by=popularity.desc&vote_average.gte=5&vote_average.lte=10&with_genres={genre}&"
-               f"without_genres={', '.join(str(x) for x in tmdb_genres.values())}") #convert dict values to a str
-        response = requests.get(url)
-        data = json.loads(response.text)
-        merged_list = merged_list + data['results']
-    return merged_list
+    for page in range(pages): #the api returns only 1 page (loop to get other pages)
+        url = f"https://api.themoviedb.org/3/discover/movie"
+        params_api = {
+            "api_key": API_KEY,
+            "include_adult": "false",
+            "include_video": "false",
+            "language": "en-US",
+            "page": page + 1,
+            "sort_by": "popularity.desc",
+            "vote_average.gte": 5,
+            "vote_average.lte": 10,
+            "with_genres": genre_ids,
+            "without_genres": ','.join(map(str, TMDB_GENRES.values()))
+        }
+        response = requests.get(url, params=params_api)
+        if response.status_code == 200:
+            data = response.json()
+            movies.extend(data.get('results', []))
+        else:
+            print("Error fetching movies:", response.text)
+    return movies
 
 def decide_for_me(user_request):
     """
-
-    :param user_request: a sting indicating if the user wants a movie or a food.
-    :return: food/movie chosen randomly. For movies, the user should filter by genre(s)
+    :param user_request: a string indicating the genre(s) of  movie.
+    :return: randomly chosen movie based on genre(s)
     """
-    if user_request.lower() == "food": #if you want to return food
-        rnd_food = random.choice(foods)
-        print("I think you should have", rnd_food, "today")
-    elif user_request.lower() == "movie": # if movie
-        genres = input("Which kind(s) of movie do you like? ")
-        if "," in genres: #in case you want more than 1 genre
-            criteria = input("Do you mean multi genre movies (both genres at the same movie) or movies with only one genre? (answer: 'multi genre' or 'mono genre') ")
-            if criteria == 'multi genre': #if you want, for example, genre 1 AND genre 2
-                genres = list(genres.split(", "))
-                genres = [genre.title() for genre in genres]
-                desirable_genre = [tmdb_genres.pop(item) for item in genres]
-                desirable_genre = ",".join(str(id) for id in desirable_genre)
-            else: # if you want genre 1 OR genre 2
-                genres = list(genres.split(", "))
-                genres = [genre.title() for genre in genres]
-                desirable_genre = [tmdb_genres.pop(item) for item in genres]
-                desirable_genre = "|".join(str(id) for id in desirable_genre)
-        else: #only one genre
-            genres = genres.capitalize()
-            desirable_genre = tmdb_genres.pop(genres)
-        movie_list = movie_names(desirable_genre, 10) #each page returns 20 movies
-        rnd_movie = random.choice(movie_list)
-        print(f"I think you should watch '{rnd_movie['title']}', released on {rnd_movie['release_date']}")
-    else:
-        print("This is not 'food' or 'movie'")
+    if "," in user_request: #in case you want more than 1 genre
+        criteria = input("Do you want multi genre movies (both genres at the same movie) or movies with only one genre? (answer: 'multi genre' or 'mono genre') ")
+        if criteria == 'multi genre': #if you want, for example, genre 1 AND genre 2
+            genres = list(user_request.split(", "))
+            genres = [genre.title() for genre in genres]
+            desirable_genre = [TMDB_GENRES.pop(item) for item in genres]
+            desirable_genre = ",".join(str(id) for id in desirable_genre)
+        else: # if you want genre 1 OR genre 2
+            genres = list(user_request.split(", "))
+            genres = [genre.title() for genre in genres]
+            desirable_genre = [TMDB_GENRES.pop(item) for item in genres]
+            desirable_genre = "|".join(str(id) for id in desirable_genre)
+    else: #only one genre
+        genres = user_request.capitalize()
+        desirable_genre = TMDB_GENRES.pop(genres)
+    movie_list = get_movie_names(desirable_genre, 10) #each page returns 20 movies
+    rnd_movie = random.choice(movie_list)
+    print(f"I think you should watch '{rnd_movie['title']}', released on {rnd_movie['release_date']}")
 
-user_request = input("What do you want me to choose for you: movie or food? ")
-
+#user_request = "Comedy, drama"
 if __name__ == '__main__':
-    decide_for_me(user_request)
+    decide_for_me(input("Which genre(s) of movie do you like? (Separate by commas): "))
